c774fbcda7ac767556139cf75d0d44e01d8fd447
rafaelguerrah <rafael_63_@hotmail.com>
2025-04-02 06:37:47 -0300

    chore(init)

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const carrosRoutes = require('./routs/carros'); 
 
-
-const port = process.env.PORT || 3000;
-
+const port = 3000;
 
 const connection = require('./config/database');
 
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors()); 
 
-// Rotas
+// Rota principal
 app.get('/', (req, res) => {
     res.status(200).json({
         message: "API em funcionamento",
@@ -20,17 +20,17 @@ app.get('/', (req, res) => {
     });
 });
 
-/
+// Usando as rotas de carros
+app.use('/carros', carrosRoutes);
+
+// rotas nao encontrada
 app.use((req, res) => {
     res.status(404).json({ message: "Rota não encontrada" });
 });
 
-
 app.listen(port, () => {
     console.log(`Servidor rodando na porta: ${port}`);
-    console.log(`Acesse: http://localhost:${port}`);
 });
 
 
-module.exports = app;
 
